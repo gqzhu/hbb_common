e8097ee11d2f1d845bcebc5cb423356c2c3487df
gqzhu9909 <gqzhu9909@aliyun.com>
2025-04-24 15:06:32 +0800

    修改中继服务器域名和KEY

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.zhuguoqiang.top"];
+pub const RS_PUB_KEY: &str = "3JtLY5+RpXxL5YPfLyu05tcuyMYmbF9MHTRbjrDs2rQ=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
